12b8617f1dfdf85b596a473160051830842cd8f1
krishnendu Dakshi <kdakshi2020@gamil.comi>
2022-01-17 14:09:32 +0530

    Initial commit

---
+++ Learnings/BitManupulation/DivideIntegers.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define NEWLINE cout<<endl;
+using namespace std;
+
+int Divide(int A,int B)
+{
+    
+}
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    
+    return 0;
+}
