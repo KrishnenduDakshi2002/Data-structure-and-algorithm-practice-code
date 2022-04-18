70ab942ff36a4068c33638fa77777a31f141258e
krishnendu Dakshi <kdakshi2020@gamil.com>
2022-04-18 08:31:45 +0530

    commits

---
+++ Algorithm/sieveOfEratosthenes.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define NEWLINE cout<<endl;
+#define ff(i,a,b) for(int i=a;i<b;i++)
+using namespace std;
+
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    
+    return 0;
+}
